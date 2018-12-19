b55169b6b714cc5b4601b7fc431019d6e9a38bf5
McJty <mcjty1@gmail.com>
2018-12-19 05:54:22 +0100

    Render fload fuild in gui of the floader

--- src/main/java/mcjty/mymod/floader/GuiFloader.java
+++ src/main/java/mcjty/mymod/floader/GuiFloader.java
@@ -1,8 +1,11 @@
 package mcjty.mymod.floader;
 
+import mcjty.mymod.ModLiquids;
 import mcjty.mymod.MyMod;
+import mcjty.mymod.tools.FluidStackRenderer;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fluids.FluidStack;
 
 public class GuiFloader extends GuiContainer {
 
@@ -10,15 +13,12 @@ public class GuiFloader extends GuiContainer {
     public static final int HEIGHT = 152;
 
     private static final ResourceLocation background = new ResourceLocation(MyMod.MODID, "textures/gui/floader.png");
-    private TileFloader furnace;
 
     public GuiFloader(TileFloader tileEntity, ContainerFloader container) {
         super(container);
 
         xSize = WIDTH;
         ySize = HEIGHT;
-
-        furnace = tileEntity;
     }
 
     @Override
@@ -31,6 +31,7 @@ public class GuiFloader extends GuiContainer {
     public void drawScreen(int mouseX, int mouseY, float partialTicks) {
         drawDefaultBackground();
         super.drawScreen(mouseX, mouseY, partialTicks);
+        FluidStackRenderer.renderFluidStack(new FluidStack(ModLiquids.fload, 1000), guiLeft + 30, guiTop + 26);
         renderHoveredToolTip(mouseX, mouseY);
     }
 }
